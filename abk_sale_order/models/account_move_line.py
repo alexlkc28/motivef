25c87b026b70c8962691f3328976bb4b41733d27
Jacky Tran <tdhungit@gmail.com>
2022-05-13 15:34:03 +0700

    #4

---
+++ abk_sale_order/models/account_move_line.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, _, fields
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    sequence = fields.Integer(compute='_compute_item_sequence', store=True, readonly=True)
+
+    @api.depends('sequence', 'move_id')
+    def _compute_item_sequence(self):
+        for record in self:
+            for move in record.mapped('move_id'):
+                number = 1
+                for line in move.move_line:
+                    line.sequence = number
+                    number += 1
