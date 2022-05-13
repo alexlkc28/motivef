a06374bf3900f13968fc7ba89de59f0ffbc70bb3
Jacky Tran <tdhungit@gmail.com>
2022-05-13 15:26:58 +0700

    #4

--- abk_sale_order/models/sale_order_line.py
+++ abk_sale_order/models/sale_order_line.py
@@ -10,7 +10,7 @@ _logger = logging.getLogger(__name__)
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    sequence = fields.Integer(compute='_compute_item_sequence', store=True)
+    sequence = fields.Integer(compute='_compute_item_sequence', store=True, readonly=True)
 
     @api.depends('sequence', 'order_id')
     def _compute_item_sequence(self):
