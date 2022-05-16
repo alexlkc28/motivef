60383c0dc02b018b9df4776dc24965b872cb5871
Jacky <tdhungit@gmail.com>
2022-05-16 11:45:37 +0700

    fixed name

--- abk_custom/__manifest__.py
+++ abk_custom/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-	'name': 'ABK Sale Order',
+	'name': 'ABK Custom Code',
 	'author': 'Aboutknowledge',
 	'version': '14.0.0.0',
 	'live_test_url': ' ',
-	'summary': 'ABK Sale Order',
-	'description': """ABK Sale Order""",
+	'summary': 'ABK Custom Code',
+	'description': """ABK Custom Code""",
 	'license': "OPL-1",
-	'depends': ['account', 'sale'],
+	'depends': ['account', 'sale', 'base'],
 	'data': [
 
 	],
