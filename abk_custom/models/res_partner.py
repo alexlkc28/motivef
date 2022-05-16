5224895d2a4889829280850b65e59ff85d0ace6b
Jacky <tdhungit@gmail.com>
2022-05-16 13:57:20 +0700

    fixed address format

--- abk_custom/models/res_partner.py
+++ abk_custom/models/res_partner.py
@@ -9,3 +9,7 @@ _logger = logging.getLogger(__name__)
 
 class AbkResPartner(models.Model):
     _inherit = 'res.partner'
+
+    @api.model
+    def _get_default_address_format(self):
+        return "%(street)s, %(street2)s %(city)s %(state_code)s %(zip)s, %(country_name)s"
