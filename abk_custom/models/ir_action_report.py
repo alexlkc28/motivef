11829ea4fea66082d36e050277f199c05ab9eef8
Jacky <tdhungit@gmail.com>
2022-05-19 13:50:41 +0700

    #9

--- abk_custom/models/ir_action_report.py
+++ abk_custom/models/ir_action_report.py
@@ -14,21 +14,25 @@ class ABKIRActionReport(models.Model):
 
     def _post_pdf(self, save_in_attachment, pdf_content=None, res_ids=None):
         if self.model == 'sale.order' and res_ids and len(res_ids) == 1:
+            reader_buffer = io.BytesIO(pdf_content)
+            reader = PdfFileReader(reader_buffer)
+            writer = PdfFileWriter()
+            writer.cloneReaderDocumentRoot(reader)
+
             so = self.env['sale.order'].browse(res_ids)
             for line in so.order_line:
                 pdf_file = line.product_id.product_tmpl_id.product_specification
-                _logger.info(pdf_file)
-            # Add attachment.
-            # reader_buffer = io.BytesIO(pdf_content)
-            # reader = PdfFileReader(reader_buffer)
-            # writer = PdfFileWriter()
-            # writer.cloneReaderDocumentRoot(reader)
-            # writer.addAttachment('factur-x.xml', xml_content)
-            # buffer = io.BytesIO()
-            # writer.write(buffer)
-            # pdf_content = buffer.getvalue()
-            #
-            # reader_buffer.close()
-            # buffer.close()
+                if pdf_file:
+                    file_reader = PdfFileReader(pdf_file)
+                    for page_num in range(file_reader.numPages):
+                        page_obj = file_reader.getPage(page_num)
+                        writer.addPage(page_obj)
+
+            buffer = io.BytesIO()
+            writer.write(buffer)
+            pdf_content = buffer.getvalue()
+
+            reader_buffer.close()
+            buffer.close()
 
         return super(ABKIRActionReport, self)._post_pdf(save_in_attachment, pdf_content, res_ids)
