a30aef4376ffb59d42db1b3533e97a74c9d4b257
Jacky <tdhungit@gmail.com>
2022-05-19 12:10:53 +0700

    #9

--- abk_custom/models/ir_action_report.py
+++ abk_custom/models/ir_action_report.py
@@ -15,8 +15,9 @@ class ABKIRActionReport(models.Model):
     def _post_pdf(self, save_in_attachment, pdf_content=None, res_ids=None):
         if self.model == 'sale.order' and res_ids and len(res_ids) == 1:
             so = self.env['sale.order'].browse(res_ids)
-            _logger.info(so)
-            _logger.info(so.order_line)
+            for line in so.order_line:
+                pdf_file = line.product_id.product_tmpl_id.product_specification
+                _logger.info(pdf_file)
             # Add attachment.
             # reader_buffer = io.BytesIO(pdf_content)
             # reader = PdfFileReader(reader_buffer)
