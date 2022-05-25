476afcee3a6fd49e77bc4c64b194aa3f1c308954
Jacky <tdhungit@gmail.com>
2022-05-25 11:01:43 +0700

    #9

--- abk_custom/models/account_move_line.py
+++ abk_custom/models/account_move_line.py
@@ -10,7 +10,7 @@ _logger = logging.getLogger(__name__)
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    sequence = fields.Integer(compute='_compute_item_sequence', store=True, readonly=True)
+    sequence = fields.Integer(store=True, readonly=True)
 
     @api.depends('sequence', 'move_id')
     def _compute_item_sequence(self):
@@ -21,9 +21,44 @@ class AccountMoveLine(models.Model):
                     line.sequence = number
                     number += 1
 
-    @api.onchange('sequence')
-    def on_change_sequence(self):
-        _logger.info('on_change_sequence')
-        for changed_line in self:
-            changed_line.sequence = 0
-        return True
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        for line in self:
+            if not line.product_id or line.display_type in ('line_section', 'line_note'):
+                continue
+
+            line.name = line._get_computed_name()
+            line.account_id = line._get_computed_account()
+            line.tax_ids = line._get_computed_taxes()
+            line.product_uom_id = line._get_computed_uom()
+            line.price_unit = line._get_computed_price_unit()
+
+            # Manage the fiscal position after that and adapt the price_unit.
+            # E.g. mapping a price-included-tax to a price-excluded-tax must
+            # remove the tax amount from the price_unit.
+            # However, mapping a price-included tax to another price-included tax must preserve the balance but
+            # adapt the price_unit to the new tax.
+            # E.g. mapping a 10% price-included tax to a 20% price-included tax for a price_unit of 110 should preserve
+            # 100 as balance but set 120 as price_unit.
+            if line.tax_ids and line.move_id.fiscal_position_id:
+                line.price_unit = line._get_price_total_and_subtotal()['price_subtotal']
+                line.tax_ids = line.move_id.fiscal_position_id.map_tax(line.tax_ids._origin,
+                                                                       partner=line.move_id.partner_id)
+                accounting_vals = line._get_fields_onchange_subtotal(price_subtotal=line.price_unit,
+                                                                     currency=line.move_id.company_currency_id)
+                balance = accounting_vals['debit'] - accounting_vals['credit']
+                line.price_unit = line._get_fields_onchange_balance(balance=balance).get('price_unit', line.price_unit)
+
+            # Convert the unit price to the invoice's currency.
+            company = line.move_id.company_id
+            line.price_unit = company.currency_id._convert(line.price_unit, line.move_id.currency_id, company,
+                                                           line.move_id.date)
+
+            for move in line.mapped('move_id'):
+                number = 1
+                for subline in move.invoice_line_ids:
+                    subline.sequence = number
+                    number += 1
+
+        if len(self) == 1:
+            return {'domain': {'product_uom_id': [('category_id', '=', self.product_uom_id.category_id.id)]}}
