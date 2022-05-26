292284e6ec222cdf34d15b1b06c016f958e1f30a
Jacky <tdhungit@gmail.com>
2022-05-26 16:52:21 +0700

    quickbook integration

--- quickbooks/__manifest__.py
+++ quickbooks/__manifest__.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-	'name': 'Quickbooks Integration',
-	'author': 'UP5TECH',
-	'version': '14.0.0.0',
-	'live_test_url': ' ',
-	'summary': 'Quickbooks Integration',
-	'description': """Quickbooks Integration""",
-	'license': "OPL-1",
-	'depends': ['account', 'sale', 'base'],
-	'data': [
-		'views/qbi_settings.xml',
-	],
-	'installable': True,
-	'auto_install': False,
-	'category': 'Customizations',
+    'name': 'Quickbooks Integration',
+    'author': 'UP5TECH',
+    'version': '14.0.0.0',
+    'live_test_url': ' ',
+    'summary': 'Quickbooks Integration',
+    'description': """Quickbooks Integration""",
+    'license': "OPL-1",
+    'depends': ['account', 'sale', 'base'],
+    'data': [
+        'views/qbi_settings.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'category': 'Customizations',
 
-	'external_dependencies': {
-        'python': ['intuit-oauth', 'python-quickbooks', 'urllib3', 'dateutil']
+    'external_dependencies': {
+        'python': ['urllib3', 'dateutil', 'intuit-oauth', 'python-quickbooks']
     }
 }
