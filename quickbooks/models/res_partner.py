f3f780d496ca2bba0abdc387d4826be35c0ae413
Jacky <tdhungit@gmail.com>
2022-05-27 16:21:15 +0700

    quickbook integration

--- quickbooks/models/res_partner.py
+++ quickbooks/models/res_partner.py
@@ -6,4 +6,4 @@ from odoo import models, api, _, fields
 class UP5QuickBooksResPartner(models.Model):
     _inherit = 'res.partner'
 
-    # quickbooks_id = fields.Char('QuickBooks ID', store=True, readonly=True)
+    quickbooks_id = fields.Char('QuickBooks ID', store=True, readonly=True)
