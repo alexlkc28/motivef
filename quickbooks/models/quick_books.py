4e6d669488ae9df2eb2d3d4a16c798439b0098fd
Jacky <tdhungit@gmail.com>
2022-05-27 10:25:44 +0700

    quickbook integration

---
+++ quickbooks/models/quick_books.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, _, fields
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class QuickBooks(models.Model):
+    _name = 'quickbooks.quickbooks'
+    _description = 'QuickBooks Model'
+
+    def get_config(self):
+        param = self.env['ir.config_parameter'].sudo()
+        return {
+            'URL': param.get_param('qbi.qk_api_url') or None,
+            'CLIENT_ID': param.get_param('qbi.qk_client_id') or None,
+            'CLIENT_SECRET': param.get_param('qbi.qk_client_secret') or None,
+            'ENVIRONMENT': param.get_param('qbi.qk_environment') or 'sandbox',
+            'REALM_ID': param.get_param('qbi.realm_id') or None,
+            'ACCESS_TOKEN': param.get_param('qbi.access_token') or None,
+            'REFRESH_TOKEN': param.get_param('qbi.refresh_token') or None,
+            'ID_TOKEN': param.get_param('qbi.id_token') or None,
+        }
+
+    def set_config(self, key, value):
+        param = self.env['ir.config_parameter'].sudo()
+        config_param = 'qbi.' + str(key)
+        param.set_param(config_param, value)
--- quickbooks/models/res_config_settings.py
+++ quickbooks/models/res_config_settings.py
@@ -10,14 +10,22 @@ _logger = logging.getLogger(__name__)
 class QBResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    qk_api_url = fields.Char(string='QuickBooks Api Url', store=True,
-                             config_parameter="qbi.qk_api_url")
-    qk_client_id = fields.Char(string='QuickBooks Client ID', store=True,
-                               config_parameter="qbi.qk_client_id")
-    qk_client_secret = fields.Char(string='QuickBooks Client Secret', store=True,
-                                   config_parameter="qbi.qk_client_secret")
+    qk_api_url = fields.Char(string='Api Url', store=True, config_parameter="qbi.qk_api_url")
+    qk_client_id = fields.Char(string='Client ID', store=True, config_parameter="qbi.qk_client_id")
+    qk_client_secret = fields.Char(string='Client Secret', store=True, config_parameter="qbi.qk_client_secret")
     qk_environment = fields.Selection([
         ('sandbox', 'Sandbox'),
         ('production', 'Production')
-    ], string='QuickBooks Environment', store=True,
-        config_parameter="qki.qk_environment")
+    ], string='QuickBooks Environment', store=True, config_parameter="qki.qk_environment")
+
+    access_token = fields.Char(string='Access Token', store=True, config_parameter="qbi.access_token")
+    refresh_token = fields.Char(string='Refresh Token', store=True, config_parameter="qbi.refresh_token")
+    id_token = fields.Char(string='ID Token', store=True, config_parameter="qbi.id_token")
+    realm_id = fields.Char(string='Realm ID', store=True, config_parameter="qbi.realm_id")
+
+    def button_login_quickbooks(self):
+        return {
+            'type': 'ir.actions.act_url',
+            'url': '/quickbooks/oauth-login/',
+            'target': 'new',
+        }
